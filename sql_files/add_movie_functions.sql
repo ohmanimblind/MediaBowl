736a436d43ece852931b11b65eda9142cb36c452
arrdyas <arrdyas@gmail.com>
2023-02-16 09:11:42 -0800

    Finished adding movie functions for database

--- sql_files/add_movie_functions.sql
+++ sql_files/add_movie_functions.sql
@@ -14,3 +14,15 @@ CREATE PROCEDURE add_movie_genre(
                             IN movie_genre varchar(50))
 	INSERT INTO movie_genre(movie_id, genre)
 			VALUES ((SELECT movie_id FROM movies WHERE movie_title = title), movie_genre);
+
+CREATE PROCEDURE add_movie_director(
+							IN movie_title varchar(50),
+                            IN movie_director varchar(50))
+	INSERT INTO director(movie_id, genre)
+			VALUES ((SELECT movie_id FROM movies WHERE movie_title = title), movie_genre);
+
+CREATE PROCEDURE add_movie_actor(
+							IN movie_title varchar(50),
+                            IN movie_director varchar(50))
+	INSERT INTO director(movie_id, actor)
+			VALUES ((SELECT movie_id FROM movies WHERE movie_title = title), movie_genre);
