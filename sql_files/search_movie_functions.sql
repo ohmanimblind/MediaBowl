ca84a15064809704832e2913b5b4f46484726376
arrdyas <arrdyas@gmail.com>
2023-02-21 17:58:28 -0800

    Reverted search movie functions

--- sql_files/search_movie_functions.sql
+++ sql_files/search_movie_functions.sql
@@ -1,19 +1,19 @@
 CREATE PROCEDURE search_movie_title(
 										IN movie_title varchar(50))
-		SELECT DISTINCT title, release_year, rating, description, runtime FROM movies
+		SELECT title, release_year, rating, description, runtime FROM movies
         WHERE movie_title = title;
 
 CREATE PROCEDURE search_movie_director(
 										IN movie_director varchar(50))
-		SELECT DISTINCT title, release_year, rating, description, runtime FROM movies
+		SELECT title, release_year, rating, description, runtime FROM movies
         WHERE movie_id IN (SELECT movie_id FROM director WHERE movie_director = director);
 
 CREATE PROCEDURE search_movie_actor(
 										IN movie_actor varchar(50))
-		SELECT DISTINCT title, release_year, rating, description, runtime FROM movies
+		SELECT title, release_year, rating, description, runtime FROM movies
         WHERE movie_id IN (SELECT movie_id FROM actor WHERE movie_actor = actor);
 
 CREATE PROCEDURE search_movie_genre(
 										IN movie_genre varchar(50))
-		SELECT DISTINCT title, release_year, rating, description, runtime FROM movies
+		SELECT title, release_year, rating, description, runtime FROM movies
         WHERE movie_id IN (SELECT movie_id FROM movie_genre WHERE movie_genre = genre);
