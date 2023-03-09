8e3b02f758c29b4dc86e681b8a54af9b4f59c3d4
ohmanimblind <apadillaap2423@gmail.com>
2023-03-08 17:03:04 -0800

    Functionality for Search Class

--- include/SearchClass.h
+++ include/SearchClass.h
@@ -15,6 +15,25 @@ void SearchBookByTitle(string title);
 void SearchMovieByTitle(string title);
 void SearchShowByTitle(string title);
 
+void SearchShowByGenre(string genre);
+void SearchBookByGenre(string genre);
+void SearchMovieByGenre(string genre);
+
+void SearchBookByAuthor(string author);
+void SearchMovieByDirector(string director);
+void SearchShowByDirector(string director);
+
+void SearchShowByActor(string actor);
+void SearchMovieByActor(string actor);
+
+void SearchBookByYear(int year);
+void SearchMovieByYear(int year);
+void SearchShowByYear(int year);
+
+// void getBookRec(string title);
+// void getShowRec(string title);
+// void getMovieRec(string title);
+
 };
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,8 @@ SearchClass searching(bookFile, movieShowFile);
 searching.SearchBookByTitle("Crime and Punishment");
 searching.SearchMovieByTitle("Magic Mike");
 searching.SearchShowByTitle("Gilmore Girls");
+searching.SearchBookByGenre("Crime");
+searching.SearchMovieByActor("Channing Tatum");
 
 return 0;
 
